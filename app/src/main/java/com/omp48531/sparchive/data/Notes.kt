81145b78835e45a9275b400fa05926f195e1cba3
Om Pandey <omp48531@gmail.com>
2025-12-12 00:47:57 +0530

    Customized Note card and added more fields in Notesviewmodel

--- app/src/main/java/com/omp48531/sparchive/data/Notes.kt
+++ app/src/main/java/com/omp48531/sparchive/data/Notes.kt
@@ -3,13 +3,17 @@ package com.omp48531.sparchive.data
 data class Note(
     val id:Int,
     val title:String ,
+    val pages: Int,
     val data:String,
+    val memberCount:Int
 )
 
 val dummyNotes=listOf<Note>(
-    Note(1,"Devki Villlage","Village is great place to live in"),
-    Note(1,"naini ","Village is great place to live in"),
-    Note(1,"Teliyerganj","Village is great place to live in"),
-    Note(1,"Village","Village is great place to live in"),
+    Note(1,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
+    Note(2,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
+    Note(3,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
+    Note(4,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
+    Note(1,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
+    Note(1,"Devki Villlage",pages=5,"Village is great place to live in", memberCount = 4),
 
 )
--- app/src/main/java/com/omp48531/sparchive/screens/NoteListScreen.kt
+++ app/src/main/java/com/omp48531/sparchive/screens/NoteListScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
@@ -64,14 +65,18 @@ fun NoteListScreen(
                 )
         }
     ) { innerPadding -> Box(
-        modifier= Modifier.background(color = Color.White).padding(20.dp)
+        modifier= Modifier
+            .background(color = Color.White)
+            .padding(20.dp)
     ){
         LazyColumn(
             Modifier.background(Color.Transparent),
             contentPadding = innerPadding
         ) {
             items(notes) { note ->
-                Box(Modifier.height(10.dp).background(color=Color.Transparent))
+                Box(Modifier
+                    .height(10.dp)
+                    .background(color = Color.Transparent))
                 NoteItem(note = note, onClick = { onNoteClick(note.id) })
             }
         }
@@ -88,22 +93,38 @@ fun NoteItem(note: com.omp48531.sparchive.data.Note, onClick: () -> Unit) {
     Box(
         modifier = Modifier
 
-
+            .clickable(onClick = onClick)
             .fillMaxWidth()
 
-            .shadow(elevation = 5.dp, shape = RoundedCornerShape(20.dp), ambientColor = Color.Black, spotColor = Color.Black)
-            .height(90.dp)
-            .background(shape = RoundedCornerShape(10.dp) , color = Color.LightGray)
+            .shadow(
+                elevation = 2.dp,
+                shape = RoundedCornerShape(10.dp),
+                ambientColor = Color.Black,
+                spotColor = Color.Black
+            )
+            .height(130.dp)
+            .background(shape = RoundedCornerShape(10.dp), color = Color(230, 230, 230))
             .padding(vertical = 20.dp, horizontal = 20.dp)
-            .clickable(onClick=onClick)
+
 
     ){
         Column {
             Text(text = note.title,
-                fontSize = 20.sp,
+                fontSize = 25.sp,
                 fontWeight = FontWeight.Bold
                 )
-            Text(text =note.data , maxLines = 1 )
+            Text(text ="${note.pages} Pages", maxLines = 1 )
+            Text(text ="${note.memberCount} Members", maxLines = 1 )
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Text("Created ON : 16-10-25",
+                    fontWeight = FontWeight.Bold, fontSize = 10.sp)
+            }
         }
     }
 //    ListItem(
